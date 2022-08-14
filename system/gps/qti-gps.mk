c3c6d17e20ff29ea9badb695652b43dcf99e149b
Jake Weinstein <jake@aospa.co>
2022-08-15 02:00:38 +0900

    gps: Define backup NTP server property
    Change-Id: I3b6427b66217d03b810a3ee988eed7d9ba3583d5

--- system/gps/qti-gps.mk
+++ system/gps/qti-gps.mk
@@ -29,5 +29,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
+# Properties
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.backup.ntpServer="0.pool.ntp.org"
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/system/gps/gps-vendor.mk)
