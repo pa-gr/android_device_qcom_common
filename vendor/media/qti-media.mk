5a7dd47d5268ee26730bbc2c268bd28975acc4a5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-06-08 21:56:48 +0530

    fixup! common: Stop explicitly building shared libs

--- vendor/media/qti-media.mk
+++ vendor/media/qti-media.mk
@@ -20,11 +20,6 @@ $(call inherit-product-if-exists, hardware/qcom/media/product.mk)
 PRODUCT_VENDOR_PROPERTIES += \
     ro.mediaserver.64b.enable=true
 
-# Packages
-PRODUCT_PACKAGES += \
-    android.hardware.media.c2@1.2.vendor \
-    libavservices_minijail.vendor
-
 # Properties
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
